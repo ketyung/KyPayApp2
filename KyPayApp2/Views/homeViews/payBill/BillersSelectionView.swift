218b99809806d20af816afb35db8d8da14aa8816
techchee <ketyung@techchee.com>
2021-07-01 16:32:58 +0800

    Update...x.x.x.x.x

--- KyPayApp2/Views/homeViews/payBill/BillersSelectionView.swift
+++ KyPayApp2/Views/homeViews/payBill/BillersSelectionView.swift
@@ -63,6 +63,8 @@ extension BillersSelectionView {
                 
                 Spacer()
                 
+                // let _ = print("biller.bid::\(biller.serviceBid ?? "xxx")")
+                
                 Common.disclosureIndicator()
               
             }
