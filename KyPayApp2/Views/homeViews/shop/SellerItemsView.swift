40fe630a49e726e4e9ed7777c190d9249a5c0969
techchee <ketyung@techchee.com>
2021-07-08 13:35:34 +0800

    Update....x.x.x

--- KyPayApp2/Views/homeViews/shop/SellerItemsView.swift
+++ KyPayApp2/Views/homeViews/shop/SellerItemsView.swift
@@ -18,6 +18,7 @@ struct SellerItemsView : View {
    
     @EnvironmentObject private var cartViewModel : CartViewModel
     
+    
     var body: some View {
         
         view()
@@ -32,32 +33,11 @@ struct SellerItemsView : View {
             
             VStack(alignment: .leading, spacing: 3){
             
-                HStack(spacing: 10) {
-               
-                    Text("KyShop - shop here for great items").font(.custom(Theme.fontNameBold, size: 15))
-                    
-                    Common.cartBadge(cartViewPresented: $control.cartViewPresented, total: cartViewModel.total(),
-                                     maxSize: CGSize(width: 70, height:34), imageSize: CGSize(width: 18, height: 14),
-                                     fontName: Theme.fontName)
-                    Spacer()//.frame(width:2)
-                }
-                .padding(.leading, 10).padding(.top, 4).padding(.bottom, 4)
-                .frame(width: UIScreen.main.bounds.width - 10)
-                .background(Color(UIColor(hex: "#ddddddff")!))
                 
-                ForEach(itemsViewModel.categoryIDs, id:\.self){
-                    cat in
-                    
-                    Text(cat).font(.custom(Theme.fontNameBold, size: 18))
-                    .padding(.leading, 15).padding(.top, 4).padding(.bottom, 4)
-                    .frame(width: UIScreen.main.bounds.width - 10, alignment: .leading)
-                    .background(Color(UIColor(hex:"#ccddccff")!))
-                        
-                    if let items = itemsViewModel.byCategories[cat] {
-                        
-                        itemsView(items)
-                    }
-                }
+                shopTitleView()
+                
+                itemListView()
+             
             }
             
         }
@@ -72,6 +52,73 @@ struct SellerItemsView : View {
 
 
 extension SellerItemsView {
+    
+    private func shopTitleView() -> some View {
+        
+        HStack(spacing: 10) {
+       
+            Text("KyShop - shop here for great items").font(.custom(Theme.fontNameBold, size: 15))
+            
+            Common.cartBadge(cartViewPresented: $control.cartViewPresented, total: cartViewModel.total(),
+                             maxSize: CGSize(width: 70, height:34), imageSize: CGSize(width: 18, height: 14),
+                             fontName: Theme.fontName)
+            Spacer()//.frame(width:2)
+        }
+        .padding(.leading, 10).padding(.top, 4).padding(.bottom, 4)
+        .frame(width: UIScreen.main.bounds.width - 10)
+        .background(Color(UIColor(hex: "#ddddddff")!))
+     
+    }
+    
+    
+    @ViewBuilder
+    private func itemListView() -> some View {
+        
+        if itemsViewModel.categoryIDs.count > 0 {
+        
+            
+            ForEach(itemsViewModel.categoryIDs, id:\.self){
+                cat in
+                
+                Text(cat).font(.custom(Theme.fontNameBold, size: 18))
+                .padding(.leading, 15).padding(.top, 4).padding(.bottom, 4)
+                .frame(width: UIScreen.main.bounds.width - 10, alignment: .leading)
+                .background(Color(UIColor(hex:"#ccddccff")!))
+                    
+                if let items = itemsViewModel.byCategories[cat] {
+                    
+                    itemsView(items)
+                }
+            }
+        }
+        else {
+            
+            
+            if itemsViewModel.inProgress {
+          
+                Text("Loading items...").padding()
+                .font(.custom(Theme.fontName, size: 16))
+          
+            }
+            
+            else {
+      
+                VStack {
+           
+                    Text("Items are not available in your country or currency yet")
+                    .font(.custom(Theme.fontName, size: 16))
+            
+                }
+                .padding()
+                .frame(minWidth: UIScreen.main.bounds.width - 10, minHeight:240)
+                .border(Color.purple, width: 1, cornerRadius: 6)
+            
+            }
+           
+            
+        }
+        
+    }
     
     
     private func itemsView (_ items : [SellerItem]) -> some View {
